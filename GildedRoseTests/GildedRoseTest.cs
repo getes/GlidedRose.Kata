d361889ab45c1de4028971cb2320748c7808a035
Borja <bferrerv@cirsa.com>
2022-09-07 15:46:35 +0200

    First Test

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -1,18 +1,41 @@
-﻿using Xunit;
+﻿//using Xunit;
 using System.Collections.Generic;
+using NUnit.Framework;
 using GildedRoseKata;
+using System.Linq;
 
 namespace GildedRoseTests
 {
+    [TestFixture]
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
+        //[Fact]
+        //public void foo()
+        //{
+        //    IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        //    GildedRose app = new GildedRose(Items);
+        //    app.UpdateQuality();
+        //    Assert.Equal("fixme", Items[0].Name);
+        //}
+
+        [Test]
+        public void FirstTest() //Pasado un dia un Articulo Standard decrementa SellIn -- y Quality --
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            //Arrange
+            //Should be ONE item of each type
+            IList<Item> Items = new List<Item> { new Item { Name = "Item1", SellIn = 10, Quality = 50 } };
             GildedRose app = new GildedRose(Items);
+
+            //Act
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+
+            //Assert
+            Assert.That(Items.Where(i=> i.Name == "Item1").FirstOrDefault().Quality , Is.EqualTo(49));
+            Assert.That(Items.Where(i => i.Name == "Item1").FirstOrDefault().SellIn, Is.EqualTo(9));
         }
+
+        //Calidad de un artículo nunca negativa
+
+        //Pasado un dia un Articulo Standard decrementa SellIn -- y Quality --
     }
 }
