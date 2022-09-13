cbf18c8146f91f41f9db990744e0913c4e7d054f
Borja <bferrerv@cirsa.com>
2022-09-13 15:40:59 +0200

    test names

--- GildedRoseTests/ApprovalTest.cs
+++ GildedRoseTests/ApprovalTest.cs
@@ -8,24 +8,24 @@ using System.Threading.Tasks;
 
 using VerifyXunit;
 
-using Xunit;
+//using Xunit;
 
 namespace GildedRoseTests
 {
     [UsesVerify]
     public class ApprovalTest
     {
-        [Fact]
-        public Task ThirtyDays()
-        {
-            var fakeoutput = new StringBuilder();
-            Console.SetOut(new StringWriter(fakeoutput));
-            Console.SetIn(new StringReader("a\n"));
+        //[Fact]
+        //public Task ThirtyDays()
+        //{
+        //    var fakeoutput = new StringBuilder();
+        //    Console.SetOut(new StringWriter(fakeoutput));
+        //    Console.SetIn(new StringReader("a\n"));
 
-            Program.Main(new string[] { });
-            var output = fakeoutput.ToString();
+        //    Program.Main(new string[] { });
+        //    var output = fakeoutput.ToString();
 
-            return Verifier.Verify(output);
-        }
+        //    return Verifier.Verify(output);
+        //}
     }
 }
--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -1,18 +1,57 @@
-﻿using Xunit;
+﻿//using Xunit;
 using System.Collections.Generic;
 using GildedRoseKata;
+using NUnit;
+using NUnit.Framework;
 
 namespace GildedRoseTests
 {
+    [TestFixture]
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+        //[Fact]
+        //public void foo()
+        //{
+        //    IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        //    GildedRose app = new GildedRose(Items);
+        //    app.UpdateQuality();
+        //    Assert.Equal("fixme", Items[0].Name);
+        //}
+
+        [Test]
+        private void test1()
+        { 
+        
         }
+
+        //Standard Item Sellin Y Quality -1 cada dia si Sellin >= 0
+        
+        //Standard Item Sellin Y Quality -2 cada dia si SellIn< 0
+        
+        //Any Item Quality nunca menor de 0
+        
+        //Si es AgedBrie Quality +1 si Sellin >= 0
+        
+        //Si es AgedBrie Quality +2 si Sellin < 0
+        
+        //AgedBrie Quality nunca mayor de 50
+        
+        //NoneItem ExceptSulfuras Has Quality over50
+        
+        //Sulfuras No modifica SellIn ni Quality && Quality == 80
+        
+        //Si BackStage SellIn< 0 --> Quality = 0
+        
+        //Si es BackStage Quality +3 si Sellin >= 0 && SellIn <= 5
+        
+        //Si es BackStage Quality +2 si Sellin > 5 && SellIn <= 10
+        
+        //Si es BackStage Quality +1 si Sellin > 10
+        
+        //BackStage Quality nunca mayor de 50
+        
+        //Si es Conjured && SellIn >= 0 Quality -2
+        
+        //Si es Conjured && SellIn< 0 Quality -4
     }
 }
