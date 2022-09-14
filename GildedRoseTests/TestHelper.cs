2e730cec9bb3f39f38461e452ad4ab6759159fbd
Borja <bferrerv@cirsa.com>
2022-09-14 14:27:58 +0200

    helper

--- GildedRoseTests/TestHelper.cs
+++ GildedRoseTests/TestHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GildedRoseKata;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,21 @@ namespace GildedRoseTests
 {
     public class TestHelper
     {
+        public const string StandardItem = "StandardItem";
+        public const string AgedBrie = "Aged Brie";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+        public const string ConjuredItem = "Conjured Mana Cake";
 
+        public static Item ItemBuilder(string name, int sellIn, int quality)
+        {
+            Item item = new () { 
+                Name = name, 
+                SellIn = sellIn, 
+                Quality = quality 
+            };
+
+            return item;
+        }
     }
 }
