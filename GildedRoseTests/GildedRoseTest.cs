c6a4935cd69cb652871af04043ffdcf6a94dbd80
Borja <bferrerv@cirsa.com>
2022-09-14 14:22:06 +0200

    testing

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -1,7 +1,6 @@
 ﻿//using Xunit;
 using System.Collections.Generic;
 using GildedRoseKata;
-using NUnit;
 using NUnit.Framework;
 
 namespace GildedRoseTests
@@ -18,40 +17,51 @@ namespace GildedRoseTests
         //    Assert.Equal("fixme", Items[0].Name);
         //}
 
-        [Test]
-        private void test1()
-        { 
-        
-        }
 
         //Standard Item Sellin Y Quality -1 cada dia si Sellin >= 0
-        
+        [Test]
+        private void StandardItemSellIn_and_QualityDecreaseByOneWhenSellinGreaterThanZero()
+        {
+            //Arrange
+
+
+
+            GildedRose app = new GildedRose();
+
+            //Act
+            app.UpdateQuality();
+
+
+            //Assert
+        }
+
+
         //Standard Item Sellin Y Quality -2 cada dia si SellIn< 0
-        
+
         //Any Item Quality nunca menor de 0
-        
+
         //Si es AgedBrie Quality +1 si Sellin >= 0
-        
+
         //Si es AgedBrie Quality +2 si Sellin < 0
-        
+
         //AgedBrie Quality nunca mayor de 50
-        
+
         //NoneItem ExceptSulfuras Has Quality over50
-        
+
         //Sulfuras No modifica SellIn ni Quality && Quality == 80
-        
+
         //Si BackStage SellIn< 0 --> Quality = 0
-        
+
         //Si es BackStage Quality +3 si Sellin >= 0 && SellIn <= 5
-        
+
         //Si es BackStage Quality +2 si Sellin > 5 && SellIn <= 10
-        
+
         //Si es BackStage Quality +1 si Sellin > 10
-        
+
         //BackStage Quality nunca mayor de 50
-        
+
         //Si es Conjured && SellIn >= 0 Quality -2
-        
+
         //Si es Conjured && SellIn< 0 Quality -4
     }
 }
